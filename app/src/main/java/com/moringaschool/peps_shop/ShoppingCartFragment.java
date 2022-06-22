b3bcf0379d0e0e2730c312dba8e5e8d438dec805
Janenjihia <jane.student@moringaschool.com>
2022-06-22 18:53:18 +0300

    Add Cart Fragment

---
+++ app/src/main/java/com/moringaschool/peps_shop/ShoppingCartFragment.java
@@ -0,0 +1,68 @@
+package com.moringaschool.peps_shop;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ShoppingCartFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ShoppingCartFragment extends Fragment implements ICheckBoxChangedListener {
+    private RecyclerView cartRecyclerView;
+    private CheckBox cbxCheckOutAll;
+    private TextView txtSubTotal, txtDeliveryCharge, txtTotal;
+    private TextView btnCheckOut;
+    private ContainerActivity activity;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View cartView = inflater.
+                inflate(R.layout.fragment_shopping_cart, container, false);
+
+        cartRecyclerView = cartView.findViewById(R.id.cart_recycler_view);
+        cbxCheckOutAll = cartView.findViewById(R.id.cbx_buy_all);
+        btnCheckOut = cartView.findViewById(R.id.check_out);
+        txtSubTotal = cartView.findViewById(R.id.cart_sub_total);
+        txtDeliveryCharge = cartView.findViewById(R.id.cart_delivery_charges);
+        txtTotal = cartView.findViewById(R.id.cart_total);
+
+        activity = (ContainerActivity) getActivity();
+        activity.cartToolbarBackground();
+
+        CartItemAdapter adapter = new CartItemAdapter(
+                getContext(),
+                AppDatabase.getInstance(getContext()).productDAO().getAllProducts()
+                , this);
+
+        cartRecyclerView.setAdapter(adapter);
+
+        cartRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        cartRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+        cbxCheckOutAll.setOnClickListener(view -> {
+            adapter.toggleAllCheckBox(cbxCheckOutAll.isChecked());
+            updateTotal(adapter.cartSumTotal(), adapter.getDeliveryCharge());
+        });
+
+        return cartView;
+    }
+
+    public void toggleCheckOutAllCheckBox(boolean isCheck) {
+        cbxCheckOutAll.setChecked(isCheck);
+    }
+
+    @Override
+    public void updateTotal(double total, double charge) {
+        DecimalFormat formatter = new DecimalFormat("#,###");
+        txtSubTotal.setText(String.format("đ %s", formatter.format(total)));
+        txtDeliveryCharge.setText(String.format("đ %s", formatter.format(charge)));
+        txtTotal.setText(String.format("đ %s", formatter.format(total + charge)));
+    }
+}
