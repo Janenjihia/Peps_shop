4451f7bcc481ca9161e220006a7c0954f744251e
Janenjihia <jane.student@moringaschool.com>
2022-06-22 19:11:25 +0300

    Add local data manager

---
+++ app/src/main/java/com/moringaschool/peps_shop/MySharedPreference.java
@@ -0,0 +1,25 @@
+package com.moringaschool.peps_shop;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MySharedPreference {
+    private static final String MY_SHARED_PREFERENCE = "MSP_DB";
+    private Context context;
+
+    public MySharedPreference(Context context) {
+        this.context = context;
+    }
+
+    public void putBoolean(String key, boolean val) {
+        context.getSharedPreferences(MY_SHARED_PREFERENCE, Context.MODE_PRIVATE)
+                .edit()
+                .putBoolean(key, val)
+                .apply();
+    }
+
+    public boolean getBoolean(String key) {
+        return context.getSharedPreferences(MY_SHARED_PREFERENCE, Context.MODE_PRIVATE)
+                .getBoolean(key, false);
+    }
+}
