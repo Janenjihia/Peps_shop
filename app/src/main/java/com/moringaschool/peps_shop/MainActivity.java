af007f5273d4fccfb106c37af889de7b32c4a994
Janenjihia <jane.student@moringaschool.com>
2022-06-22 18:19:55 +0300

    Add logic for Main Activity

--- app/src/main/java/com/moringaschool/peps_shop/MainActivity.java
+++ app/src/main/java/com/moringaschool/peps_shop/MainActivity.java
@@ -1,14 +1,30 @@
 package com.moringaschool.peps_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout mFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mFrameLayout = findViewById(R.id.fragment_container);
+        setFragment(new LoginFragment(mFrameLayout));
+    }
+
+    private void setFragment(Fragment loginActivity) {
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_right)
+                .replace(mFrameLayout.getId(), loginActivity).commit();
     }
 }
