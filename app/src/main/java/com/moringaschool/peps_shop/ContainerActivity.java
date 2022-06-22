28a7f4bbf3dd9784a117323820d88b9b4f967dbd
Janenjihia <jane.student@moringaschool.com>
2022-06-22 19:05:09 +0300

    Modify Container Activity

--- app/src/main/java/com/moringaschool/peps_shop/ContainerActivity.java
+++ app/src/main/java/com/moringaschool/peps_shop/ContainerActivity.java
@@ -1,4 +1,133 @@
 package com.moringaschool.peps_shop;
 
-public class ContainerActivity {
-}
+public class ContainerActivity extends AppCompatActivity {
+
+    public static final String CONTAINER_KEY = "container_data";
+    public static final String CONTAINER_TOOLBAR_MENU_MODE = "menu";
+
+    private FrameLayout mainContainer;
+    private AppBarLayout appBarLayout;
+    private Toolbar mToolbar;
+    private int modeMenu;
+    private Menu myMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_container);
+
+        appBarLayout = findViewById(R.id.app_bar_2);
+
+        mToolbar = findViewById(R.id.my_toolbar_2);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        productDetailToolbarBackIcon();
+
+        mainContainer = findViewById(R.id.product_container);
+
+        Bundle bundle = getIntent().getBundleExtra(CONTAINER_KEY);
+        Product product = (Product) bundle.getSerializable(ProductDetailFragment.PRODUCT_DETAIL_KEY);
+
+        int mode = (int) bundle.get(CONTAINER_TOOLBAR_MENU_MODE);
+        modeMenu = mode;
+
+        if (mode == 1)
+            setFragment(new ProductDetailFragment(product));
+        else
+            setFragment(new ShoppingCartFragment());
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .add(mainContainer.getId(), fragment)
+                .commit();
+    }
+
+    public void hideToolBarTitle() {
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    public void cartToolbarBackground() {
+        appBarLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.secondaryColor));
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back_white);
+        setToolBarTitle("My Cart");
+    }
+
+    public void setToolBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+    }
+
+    public void productDetailToolbarBackIcon() {
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+    }
+
+    public void showBackground() {
+        appBarLayout.setBackgroundColor(Color.WHITE);
+        appBarLayout.setElevation(40f);
+    }
+
+    public void hideBackground() {
+        appBarLayout.setBackground(null);
+        appBarLayout.setElevation(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        myMenu = menu;
+        if (modeMenu != 0) {
+            MenuInflater menuInflater = getMenuInflater();
+            menuInflater.inflate(R.menu.toolbar_menu, menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int selectedItemId = item.getItemId();
+        switch (selectedItemId) {
+            case R.id.toolbar_cart: {
+                myMenu.findItem(R.id.toolbar_cart).setVisible(false);
+                openCartLayout();
+                break;
+            }
+            case android.R.id.home: {
+                onBackPressed();
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0)
+            super.onBackPressed();
+        else {
+            myMenu.findItem(R.id.toolbar_cart).setVisible(true);
+            getSupportFragmentManager().popBackStack();
+        }
+    }
+
+    public void openCartLayout() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(mainContainer.getId(), new ShoppingCartFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    public void updateCartQuantityNotification(int quantity) {
+        BadgeDrawable badgeDrawable = BadgeDrawable.create(ContainerActivity.this);
+        badgeDrawable.setNumber(quantity);
+        badgeDrawable.setBackgroundColor(Color.parseColor("#ee4d2d"));
+        BadgeUtils.attachBadgeDrawable(badgeDrawable, mToolbar, R.id.toolbar_cart);
+    }
